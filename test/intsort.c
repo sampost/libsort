d5161bd0bedac97f326c26665432831879c95ead
Sam Post <sampost@gmail.com>
2010-11-27 10:08:00 -0800

    intsort: ARGV[2] used to run a specific algorithm

--- test/intsort.c
+++ test/intsort.c
@@ -14,9 +14,16 @@ int main(int argc, char ** argv)
 {
     int * numbers;
     int   len, i;
+    char * algo = NULL;
+
+    if(argc == 3)
+    {
+        algo = argv[2];
+        printf("Limiting algorithm to %s\n", algo);
+    }
 
     srand(time(NULL));
-    if(argc == 2)
+    if(argc >= 2)
         len = atoi(argv[1]);
     else
     {
@@ -33,43 +40,49 @@ int main(int argc, char ** argv)
     for(i = 0; i < len; i++)
         numbers[i] = rand();
 
-    printf("Sorting bubblesort, int asc\n");
-    bubblesort(numbers, len, sizeof(int), int_compare_asc);
+    if(algo == NULL || strcmp(algo, "bubble") == 0)
+    {
+        printf("Sorting bubblesort, int asc\n");
+        bubblesort(numbers, len, sizeof(int), int_compare_asc);
 
-    /* verify the list is sorted ascending */
-    for(i = 0; i < len-1; i++)
-        if(numbers[i] > numbers[i+1])
-            printf("Failure! n[%d] > n[%d] (%d, %d)\n",
-                    i, i+1, numbers[i], numbers[i+1]);
+        /* verify the list is sorted ascending */
+        for(i = 0; i < len-1; i++)
+            if(numbers[i] > numbers[i+1])
+                printf("Failure! n[%d] > n[%d] (%d, %d)\n",
+                        i, i+1, numbers[i], numbers[i+1]);
 
-    printf("Sorting bubblesort, int desc\n");
-    bubblesort(numbers, len, sizeof(int), int_compare_desc);
+        printf("Sorting bubblesort, int desc\n");
+        bubblesort(numbers, len, sizeof(int), int_compare_desc);
 
-    /* verify the list is sorted descending */
-    for(i = 0; i < len-1; i++)
-        if(numbers[i] < numbers[i+1])
-            printf("Failure! n[%d] < n[%d] (%d, %d)\n",
-                    i, i+1, numbers[i], numbers[i+1]);
-
-
-    printf("Sorting mergesort, int asc\n");
-    mergesort(numbers, len, sizeof(int), int_compare_asc);
+        /* verify the list is sorted descending */
+        for(i = 0; i < len-1; i++)
+            if(numbers[i] < numbers[i+1])
+                printf("Failure! n[%d] < n[%d] (%d, %d)\n",
+                        i, i+1, numbers[i], numbers[i+1]);
+    }
 
 
-    /* verify the list is sorted ascending */
-    for(i = 0; i < len-1; i++)
-        if(numbers[i] > numbers[i+1])
-            printf("Failure! n[%d] > n[%d] (%d, %d)\n",
-                    i, i+1, numbers[i], numbers[i+1]);
+    if(algo == NULL || strcmp(algo, "merge") == 0)
+    {
+        printf("Sorting mergesort, int asc\n");
+        mergesort(numbers, len, sizeof(int), int_compare_asc);
 
-    printf("Sorting mergesort, int desc\n");
-    mergesort(numbers, len, sizeof(int), int_compare_desc);
 
-    /* verify the list is sorted descending */
-    for(i = 0; i < len-1; i++)
-        if(numbers[i] < numbers[i+1])
-            printf("Failure! n[%d] < n[%d] (%d, %d)\n",
-                    i, i+1, numbers[i], numbers[i+1]);
+        /* verify the list is sorted ascending */
+        for(i = 0; i < len-1; i++)
+            if(numbers[i] > numbers[i+1])
+                printf("Failure! n[%d] > n[%d] (%d, %d)\n",
+                        i, i+1, numbers[i], numbers[i+1]);
+
+        printf("Sorting mergesort, int desc\n");
+        mergesort(numbers, len, sizeof(int), int_compare_desc);
+
+        /* verify the list is sorted descending */
+        for(i = 0; i < len-1; i++)
+            if(numbers[i] < numbers[i+1])
+                printf("Failure! n[%d] < n[%d] (%d, %d)\n",
+                        i, i+1, numbers[i], numbers[i+1]);
+    }
 
     free(numbers);
     return 0;
